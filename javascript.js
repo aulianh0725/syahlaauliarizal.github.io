b22078529e88999d3185d5c7d4e5521e3a643abe
aulianh0725 <71535989+aulianh0725@users.noreply.github.com>
2020-10-28 21:34:00 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var n = prompt("Masukan Nama Anda:");
+var c = confirm("Hai " + n + " Selamat Datang di DAPUR MAHASISWA");
+if (c == true) {
+    alert('Oh Thanks!!');
+} else {
+    alert('Why?!!');
+}
+var c = confirm(n + " Mau Tahu Resep Kami ?");
+if (c == true) {
+    alert('Selamat Mencoba:)');
+} else {
+    alert('No Problem');
+}
